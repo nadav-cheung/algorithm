ae2db098ecba88b20ea7ee43f89fee2c654f32e8
nadav <nadav.cheung@gmail.com>
2023-12-21 16:49:12 +0800

    Java LinkedList complete 30%

--- src/main/java/cn/com/nadav/structure/util/DoublyLinkedList.java
+++ src/main/java/cn/com/nadav/structure/util/DoublyLinkedList.java
@@ -1,12 +1,225 @@
 package cn.com.nadav.structure.util;
 
-public class DoublyLinkedList {
+import java.util.LinkedList;
+
+// 使用双向链表实现List数据结构
+public class DoublyLinkedList<E> implements List<E> {
+
+
+    transient int size = 0;
+
+    /**
+     * Pointer to first node.
+     */
+    transient Node<E> first;
+
+    /**
+     * Pointer to last node.
+     */
+    transient Node<E> last;
+
+    /**
+     * Constructs an empty list.
+     */
+    public DoublyLinkedList() {
+    }
+
+    /**
+     * Links e as first element.
+     */
+    private void linkFirst(E e) {
+        final Node<E> f = first;
+        final Node<E> newNode = new Node<>(null, e, f);
+        first = newNode;
+        if (f == null)
+            last = newNode;
+        else
+            f.prev = newNode;
+        size++;
+    }
+
+    /**
+     * Unlinks non-null first node f.
+     */
+    private E unlinkFirst(Node<E> f) {
+        // assert f == first && f != null;
+        final E element = f.item;
+        final Node<E> next = f.next;
+        f.item = null;
+        f.next = null; // help GC
+        first = next;
+        if (next == null)
+            last = null;
+        else
+            next.prev = null;
+        size--;
+        return element;
+    }
+
+
+    /**
+     * Links e as last element.
+     */
+    private void linkLast(E e) {
+        final Node<E> l = last;
+        final Node<E> newNode = new Node<>(l, e, null);
+        last = newNode;
+        if (l == null)
+            first = newNode;
+        else
+            l.next = newNode;
+        size++;
+    }
+
+    /**
+     * Unlinks non-null last node l.
+     */
+    private E unlinkLast(Node<E> l) {
+        // assert l == last && l != null;
+        final E element = l.item;
+        final Node<E> prev = l.prev;
+        l.item = null;
+        l.prev = null; // help GC
+        last = prev;
+        if (prev == null)
+            first = null;
+        else
+            prev.next = null;
+        size--;
+        return element;
+    }
+
+
+    @Override
+    public E get(int index) {
+        checkElementIndex(index);
+        return node(index).item;
+    }
+
+    /**
+     * 判断index位于链表的前半部还是后半部分，选择从头部还是尾部开始寻找
+     * Returns the (non-null) Node at the specified element index.
+     */
+    Node<E> node(int index) {
+        // assert isElementIndex(index);
+        if (index < (size >> 1)) {
+            Node<E> x = first;
+            for (int i = 0; i < index; i++)
+                x = x.next;
+            return x;
+        } else {
+            Node<E> x = last;
+            for (int i = size - 1; i > index; i--)
+                x = x.prev;
+            return x;
+        }
+    }
+
+    private void checkElementIndex(int index) {
+        if (!isElementIndex(index))
+            throw new IndexOutOfBoundsException(outOfBoundsMsg(index));
+    }
+
+    private String outOfBoundsMsg(int index) {
+        return "Index: " + index + ", Size: " + size;
+    }
+
+    /**
+     * Tells if the argument is the index of an existing element.
+     */
+    private boolean isElementIndex(int index) {
+        return index >= 0 && index < size;
+    }
+
+    @Override
+    public E set(int index, E e) {
+        checkElementIndex(index);
+        Node<E> x = node(index);
+        E oldVal = x.item;
+        x.item = e;
+        return oldVal;
+    }
+
+    @Override
+    public void add(int index, E e) {
+        checkPositionIndex(index);
+
+        if (index == size)
+            linkLast(e);
+        else
+            linkBefore(e, node(index));
+    }
+
+    /**
+     * Inserts element e before non-null Node succ.
+     */
+    void linkBefore(E e, Node<E> succ) {
+        // assert succ != null;
+        final Node<E> pred = succ.prev;
+        final Node<E> newNode = new Node<>(pred, e, succ);
+        succ.prev = newNode;
+        if (pred == null)
+            first = newNode;
+        else
+            pred.next = newNode;
+        size++;
+    }
+
+
+
+    private void checkPositionIndex(int index) {
+        if (!isPositionIndex(index))
+            throw new IndexOutOfBoundsException(outOfBoundsMsg(index));
+    }
+    private boolean isPositionIndex(int index) {
+        return index >= 0 && index <= size;
+    }
+
+    @Override
+    public boolean add(E e) {
+        return false;
+    }
+
+    @Override
+    public int indexOf(E e) {
+        return 0;
+    }
+
+    @Override
+    public int lastIndexOf(E o) {
+        return 0;
+    }
+
+    @Override
+    public boolean contains(E o) {
+        return false;
+    }
+
+    @Override
+    public E remove(int index) {
+        return null;
+    }
+
+    @Override
+    public boolean remove(E o) {
+        return false;
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public boolean empty() {
+        return size() == 0;
+    }
 
     // 双向链表
-    private static class Node<E> {
+    static class Node<E> {
         E item;
-        Node<E> next;
         Node<E> prev;
+        Node<E> next;
 
         Node(Node<E> prev, E element, Node<E> next) {
             this.item = element;
--- src/main/java/cn/com/nadav/structure/util/LinkedListRecursion.java
+++ src/main/java/cn/com/nadav/structure/util/LinkedListRecursion.java
@@ -1,59 +1,42 @@
 package cn.com.nadav.structure.util;
 
+/**
+ * 单项链表递归插入元素
+ */
 public class LinkedListRecursion<E> {
-
-    private Node head;
-    private int size;
-
+    private Node<E> head;
     public LinkedListRecursion() {
         head = null;
-        size = 0;
-    }
-
-    public int getSize() {
-        return size;
-    }
-
-    public boolean isEmpty() {
-        return size == 0;
     }
 
     public void add(int index, E e) {
         head = add(head, index, e);
-        size++;
     }
 
-    // 递归的宏观语义   在以node为头结点的链表的index位置插入e 并返回node节点
-    private Node add(Node node, int index, E e) {
+    // 递归的宏观语义   在以node为头结点的链表的index位置插入e
+    // 并返回插入元素后的node节点
+    private Node<E> add(Node<E> node, int index, E e) {
         if (index == 0) {
-            return new Node(e, node);
+            return new Node<E>(e, node);
         }
-
         node.next = add(node.next, index - 1, e);
         return node;
     }
 
-    public class Node {
+    static class Node<E> {
         public E e;
-        public Node next;
+        public Node<E> next;
 
-        public Node(E e, Node next) {
+        public Node(E e, Node<E> next) {
             this.e = e;
             this.next = next;
         }
 
-
         public Node(E e) {
             this(e, null);
         }
-
         public Node() {
             this(null, null);
-        }
-
-        @Override
-        public String toString() {
-            return e.toString();
         }
     }
 
